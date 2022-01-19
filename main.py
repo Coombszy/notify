c2e59001c7dec2c0cb46c30886b98d257003096c
Coombszy <LCCoombs@hotmail.co.uk>
2022-01-19 14:23:52 +0000

    Add progress so far

--- main.py
+++ main.py
@@ -1,32 +1,63 @@
-from operator import truediv
-from unittest import runner
+import datetime
+import time
 import requests
+import json
+
+# CONSTANTS
+########################################################################################################
+R_TEXT1 = '347192837'
+R_TEXT2 = '912372135'
+########################################################################################################
 
 # CONFIGS
 ########################################################################################################
 URL = 'https://maker.ifttt.com/trigger/notification_phone/with/key/dAwxxBLcB3qNYgaNR1XL2U'
 HEADERS = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-JSON = '{"value1":"REPLACEME1", "value2":"REPLACEME2"}'
+JSON = '{"value1":"' + R_TEXT1 + '", "value2":"' + R_TEXT2 + '"}'
+WAIT = 60
 VERSION = 'v0.1'
+CONFIG_LOCATION = './configs/'
 ########################################################################################################
 
 # DATA
 ########################################################################################################
-data = []
+data = {}
 
-data
-
+#   monday
+data[0] = json.load(open(CONFIG_LOCATION + '0.json'))
+#   tuesday
+data[1] = json.load(open(CONFIG_LOCATION + '1.json'))
+#   wednesday
+data[2] = json.load(open(CONFIG_LOCATION + '2.json'))
+#   thursday
+data[3] = json.load(open(CONFIG_LOCATION + '3.json'))
+#   friday
+data[4] = json.load(open(CONFIG_LOCATION + '4.json'))
+#   saturday
+data[5] = json.load(open(CONFIG_LOCATION + '5.json'))
+#   sunday
+data[6] = json.load(open(CONFIG_LOCATION + '6.json'))
 ########################################################################################################
 
+# MAIN
+########################################################################################################
 print('Starting Notify ' + VERSION)
 RUNNING = True
-# while(RUNNING):
-    
+DAY = 0 # datetime.datetime.today().weekday()
+while(RUNNING):
+
+    now = datetime.datetime.today()
+    print(DAY)
+    # If day has changed, roll over and reload config
+    if( now.weekday() != DAY):
+        # DAY = now.weekday() #####################################################UN COMMENT ME!
+        data[DAY] = json.load(open(CONFIG_LOCATION + str(DAY) +'.json'))
+
+    minutes = (now.hour*60) + now.minute
+
+    print(data[DAY])
+
+    time.sleep(WAIT)
 
 
-
-r = requests.post(URL, data=JSON, headers=HEADERS)
-
-
-
-
+# r = requests.post(URL, data=JSON, headers=HEADERS)
