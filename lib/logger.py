ef56b8543d6be1b522cee5d9c4fd493394c5d80b
Coombszy <LCCoombs@hotmail.co.uk>
2022-01-19 18:57:54 +0000

    Finish logger

--- lib/logger.py
+++ lib/logger.py
@@ -2,15 +2,47 @@
 #
 #   Logging Level table:
 #       0 - ERROR
-#       1 - WARNING
-#       2 - LOGGING
+#       1 - WARN
+#       2 - LOG
 #       3 - DEBUG
 #
 ########################################################################################################
+from datetime import datetime
+
 class Logger:
 
     def __init__(self, logging_level):
         self.level = logging_level
 
-    def log(content, level):
-        print()
+    @staticmethod
+    def timestamp():
+        return datetime.now().strftime("[%H:%M:%S]")
+
+    def write(self, content, level):
+
+        prefix = ""
+        
+        if(level == 0):
+            prefix = '[ERROR]'
+        elif (level == 1):
+            prefix = '[ WARN]'
+        elif (level == 2):
+            prefix = '[  LOG]'
+        elif (level == 3):
+            prefix = '[DEBUG]'
+        else:
+            prefix = '[ -' + str(level) + '- ]'
+        
+        print(Logger.timestamp() + prefix + content)
+
+    def error(self, content):
+        self.write(content, 0)
+
+    def warn(self, content):
+        self.write(content, 1)
+
+    def log(self, content):
+        self.write(content, 2)
+
+    def debug(self, content):
+        self.write(content, 3)
