998a55399b427b034bbd05272a51b8ca8d347871
Coombszy <LCCoombs@hotmail.co.uk>
2022-01-19 18:10:56 +0000

    Add configs Add sample config Update gitignore

--- main.py
+++ main.py
@@ -6,21 +6,20 @@ import datetime
 import time
 import requests
 import json
+import configparser
 
-# CONSTANTS
+# CONFIGS/LIBS
 ########################################################################################################
-R_TEXT1 = '347192837'
-R_TEXT2 = '912372135'
-########################################################################################################
-
-# CONFIGS
-########################################################################################################
-URL = 'https://maker.ifttt.com/trigger/notification_phone/with/key/dAwxxBLcB3qNYgaNR1XL2U'
-HEADERS = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-JSON = '{"value1":"' + R_TEXT1 + '", "value2":"' + R_TEXT2 + '"}'
-WAIT = 60
 VERSION = 'v0.1'
-CONFIG_LOCATION = './configs/'
+HEADERS = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+CONFIG_LOCATION = './config.ini'
+
+config = configparser.ConfigParser()
+config.read(CONFIG_LOCATION)
+
+URL = config['notify']['URL']
+WAIT = int(config['notify']['WAIT'])
+NOTIFICATION_LOCATION = config['notify']['NOTIFICATION_LOCATION']
 ########################################################################################################
 
 # DATA
@@ -28,42 +27,51 @@ CONFIG_LOCATION = './configs/'
 data = {}
 
 #   monday
-data[0] = json.load(open(CONFIG_LOCATION + '0.json'))
+data[0] = json.load(open(NOTIFICATION_LOCATION + '0.json'))
 #   tuesday
-data[1] = json.load(open(CONFIG_LOCATION + '1.json'))
+data[1] = json.load(open(NOTIFICATION_LOCATION + '1.json'))
 #   wednesday
-data[2] = json.load(open(CONFIG_LOCATION + '2.json'))
+data[2] = json.load(open(NOTIFICATION_LOCATION + '2.json'))
 #   thursday
-data[3] = json.load(open(CONFIG_LOCATION + '3.json'))
+data[3] = json.load(open(NOTIFICATION_LOCATION + '3.json'))
 #   friday
-data[4] = json.load(open(CONFIG_LOCATION + '4.json'))
+data[4] = json.load(open(NOTIFICATION_LOCATION + '4.json'))
 #   saturday
-data[5] = json.load(open(CONFIG_LOCATION + '5.json'))
+data[5] = json.load(open(NOTIFICATION_LOCATION + '5.json'))
 #   sunday
-data[6] = json.load(open(CONFIG_LOCATION + '6.json'))
+data[6] = json.load(open(NOTIFICATION_LOCATION + '6.json'))
 ########################################################################################################
 
 # MAIN
 ########################################################################################################
 print('Starting Notify ' + VERSION)
 RUNNING = True
-DAY = 0 # datetime.datetime.today().weekday()
+DAY = datetime.datetime.today().weekday()
 while(RUNNING):
 
     now = datetime.datetime.today()
     # If day has changed, roll over and reload config
     if( now.weekday() != DAY):
-        # DAY = now.weekday() #####################################################UN COMMENT ME!
+        DAY = now.weekday() 
         data[DAY] = json.load(open(CONFIG_LOCATION + str(DAY) +'.json'))
 
     # Get minutes passed in the day
     minutes = (now.hour*60) + now.minute
 
     for notification in data[DAY]['notifications']:
-        if 'active' in notification:
+        # If active is missing, add it 
+        if 'active' not in notification:
             notification['active'] = 'true'
 
+        # If notification is ready to send, Send!
+        if notification['active'] == 'true' and int(notification['trigger']) <= minutes:
+            # Send notification
+            data_json = '{"value1":"' + str(notification['title']) + '", "value2":"' + str(notification['content']) + '"}'
+            r = requests.post(URL, data=data_json, headers=HEADERS)
+            # If notification failed
+            if not(r.ok):
+                print("FAILED TO SEND NOTIFICATION:" + data)
+
+            notification['active'] = 'false'
+
     time.sleep(WAIT)
-
-
-# r = requests.post(URL, data=JSON, headers=HEADERS)
