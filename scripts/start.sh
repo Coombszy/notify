1c7bf5336042637bc428cedb4ca45e1eef305bb5
Coombszy <LCCoombs@hotmail.co.uk>
2022-01-21 22:36:26 +0000

    Added detached script Added testing mounts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get the path of the shell script
+SCRIPT_LOCATION=`dirname $(realpath $0)`
+
 # This script will start a notify container using latest
 echo 'Starting notify container'
-docker run -d --name notify notify:latest
+docker run -d --name notify --mount type=bind,source=$SCRIPT_LOCATION/../testing/config,target=/app/config --mount type=bind,source=$SCRIPT_LOCATION/../testing/notifications,target=/app/notifications notify:latest 
