e2e3ffde7379b67a686be9abc3191b7610d0f664
Coombszy <LCCoombs@hotmail.co.uk>
2022-05-20 21:33:34 +0100

    Logging implemented and .env

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 pub mod libs;
+
 use crate::libs::utils::{draw_start_screen, load_config_toml};
 use crate::libs::structs::TOMLData;
 
+use log::{info, warn, error, debug};
+use dotenv::dotenv;
+use std::env;
+
 
 fn main() {
+    // Init environment vars and logger
+    dotenv().ok();
+    env_logger::init();
+
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
+
     draw_start_screen();
     let config_file = "notify.toml".to_string();
 
     let config: TOMLData = load_config_toml(config_file);
-    println!("{}",config.config.url)
+    debug!("{}",config.config.url)
 
 }
 
