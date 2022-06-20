80beb2edb1c92e906bd104de9d0dce310d05ab73
Coombszy <LCCoombs@hotmail.co.uk>
2022-06-20 12:25:35 +0100

    Fixed certs issue

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -3,12 +3,14 @@
 
 echo "TARGETPLATFORM: $1"
 
+apt-get update && apt-get upgrade -qq
+apt-get install --install-recommends -y perl openssl libssl-dev librust-openssl-dev \
+	build-essential ca-certificates
+
 if [ "$1" = "linux/arm/v7" ] 
 then
-    apt-get update && apt-get upgrade -qq && \
-    apt-get install --install-recommends -y openssl libssl-dev librust-openssl-dev \
-    build-essential gcc-arm-linux-gnueabihf \
-    pkg-config
+    rm -f target/armv7-unknown-linux-gnueabihf/release/deps/notify-*
+    apt-get install --install-recommends -y gcc-arm-linux-gnueabihf pkg-config
     rustup target add armv7-unknown-linux-gnueabihf
     cargo build --release --target armv7-unknown-linux-gnueabihf; code=$?
     mkdir -p /target/release
@@ -18,6 +20,8 @@ fi
 
 if [ "$1" = "linux/amd64" ]
 then
+    # Assuming you are running this on a amd64 machine
+    rm -f target/release/deps/notify-*
     cargo build --release
     exit $?
 fi
